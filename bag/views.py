b6e89a7b16b2738590e9a4c164f9e2b1f9977f7f
brendanoshea84 <b_oshea_84@hotmail.com>
2020-07-21 17:28:08 +0200

    testing add_to_bag function

--- bag/views.py
+++ bag/views.py
@@ -13,37 +13,28 @@ def view_bag(request):
 
 def add_to_bag(request):
     """ Add a quantity of the specified product to the shopping bag """
-    print("working stage 1")
+
     redirect_url = request.POST.get('redirect_url')
     name = request.POST.get('jumper_Name')
     dob = request.POST.get('jumper_Date_Of_Birth')
 
-    a = 0
-    print(a)
-
-    print("working stage 2")
-    
     bag = request.session.get('bag', {})
 
-    print("working stage 3")
-
-    if request.method == 'POST':
-        a += 1
-        customer = {
-            "name": name,
-            "dob": dob
-        }
-        bag[a] = customer
-        
-        print("*********")
-        print(a)
-        print("*********")
-        print(customer)
-        print("*********")
-        print("test bag")
-        print(bag)
-        
-        
+    print("stage 1")
+    testing = []
     
+    if request.method == 'POST':
+        testing.append({
+            "name": name,
+            "dob": dob  
+        })   
+        bag[testing]
+        return(testing)
 
+    request.session['bag'] = bag
+    print("*******") 
+    print(testing) 
+    print("*******") 
+    print(bag)
+       
     return redirect(redirect_url)
