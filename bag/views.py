5603e504f25df6629c3c8c2223d9b66d853fc574
brendanoshea84 <b_oshea_84@hotmail.com>
2020-07-30 10:15:59 +0200

    fixed bag index with new customer

--- bag/views.py
+++ bag/views.py
@@ -14,27 +14,59 @@ def view_bag(request):
 def add_to_bag(request):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = get_object_or_404(Product, pk=1)
+
     redirect_url = request.POST.get('redirect_url')
     name = request.POST.get('jumper_Name')
     dob = request.POST.get('jumper_Date_Of_Birth')
 
     bag = request.session.get('bag', {})
-
-    print("stage 1")
-    testing = []
     
-    if request.method == 'POST':
-        testing.append({
-            "name": name,
-            "dob": dob  
-        })   
-        bag[testing]
-        return(testing)
+    print("stage 1")
 
+    customer =({'name': name,'dob': dob})
+
+    if bag == {}:
+        print("stage 1")    
+        bag[0] = customer
+
+    else:
+        print("stage 2")   
+        test = len(bag)
+        print(test)
+        bag[test] = customer
+
+    
+    print("stage 4")
     request.session['bag'] = bag
-    print("*******") 
-    print(testing) 
-    print("*******") 
+    
     print(bag)
+
+
+
+
+# if bag == {}:
+#         bag = customer
+#     else:    
+#         bag.append(customer)
+
        
+# session = [{"stuff": "things"},]
+
+# bag = []
+# customer = [{"name": "michael", "dob": "yonks ago"}, {"name": "michael2", "dob": "younger than michael"}]
+
+# if customer:
+#         for i in customer:
+#                 bag.append(i)
+
+# session.append({"bag": bag})
+
+# print(session)
+
+
+
+
+
+
     return redirect(redirect_url)
