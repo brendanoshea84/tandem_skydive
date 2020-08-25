fafa7d9078904ca4c6bcc57ecc7f6b1e21b9bb68
brendanoshea84 <b_oshea_84@hotmail.com>
2020-08-25 10:39:01 +0200

    added messages for webhook test

--- bag/webhooks.py
+++ bag/webhooks.py
@@ -34,7 +34,7 @@ def webhook(request):
         return HttpResponse(content=e, status=400)
 
 
-    print("webhook sussess")
+    messages.error(request, 'webhook working.')
     return HttpResponse(status=200)
 
     # # Set up a webhook handler
